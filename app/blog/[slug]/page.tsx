9fc3f1ab27fa2c43f8379c419562c322dbfa2611
Poshan Peeroo <poshanpeeroo@gmail.com>
2024-07-12 14:02:27 +0400

    indents

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
-import { getBlog,getBlogs } from '@/lib/blogs';
+import { getBlog, getBlogs } from '@/lib/blogs';
+import { Spinner } from '@nextui-org/spinner';
 import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
 
 export async function generateMetadata({ params }: { params: any }) {
@@ -11,23 +13,26 @@ export async function generateMetadata({ params }: { params: any }) {
 
 export async function generateStaticParams() {
     const blogs = await getBlogs()
-   
+
     return blogs.map((blog) => ({
-      slug: blog.slug,
+        slug: blog.slug,
     }))
-  }
+}
 
 export default async function BlogPage({ params }: { params: any }) {
     let { content } = await getBlog(params.slug)
-    
+
     try {
     } catch {
         notFound()
     }
     return (
-        <div className='w-full content-center justify-center prose dark:prose-invert'>
-            {content}
-        </div>
+        <Suspense fallback={<Spinner  className='w-full content-center justify-center'/>}>
+            <div className='w-full content-center justify-center prose dark:prose-invert'>
+                {content}
+            </div>
+        </Suspense>
+
     )
 }
 
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,22 +1,35 @@
-import { BlogGrid } from "@/components/blogitem";
-import { getBlogs } from "@/lib/blogs";
+// pages/blogs.tsx
+import dynamic from 'next/dynamic';
+import { Spinner } from '@nextui-org/spinner';
+import { getBlogs } from '@/lib/blogs';
+import { Suspense } from 'react';
 
-export default async function BlogsPage({ searchParams } : { searchParams: any}) {
+// Dynamically import BlogGrid
+const BlogGrid = dynamic(() => import('@/components/blogitem').then(mod => mod.BlogGrid), {
+  suspense: true
+});
+
+export default async function BlogsPage({ searchParams }: { searchParams: any }) {
   const unfilteredBlogs = await getBlogs();
-  
-  let blogs = unfilteredBlogs.sort((a, b) => {
-    return new Date(b.frontmatter.date as string).getTime() - new Date(a.frontmatter.date as string).getTime()
-  }).filter((blog) => {
-    if (searchParams.query !== undefined && searchParams.query !== '') {
-      let query = searchParams.query.toLowerCase()
-      let title = (blog.frontmatter.title as string).toLowerCase()
-      return title.includes(query)
-    }
-    return true
-  })
+
+  let blogs = unfilteredBlogs
+    .sort((a, b) => {
+      return new Date(b.frontmatter.date as string).getTime() - new Date(a.frontmatter.date as string).getTime();
+    })
+    .filter((blog) => {
+      if (searchParams.query !== undefined && searchParams.query !== '') {
+        let query = searchParams.query.toLowerCase();
+        let title = (blog.frontmatter.title as string).toLowerCase();
+        return title.includes(query);
+      }
+      return true;
+    });
+
   return (
-    <div className={`flex ${blogs.length === 0 ? 'items-center' : ''} justify-center h-full`}>
-      {blogs.length === 0 ? (<div><p className="text-2xl">No result</p></div>) : BlogGrid({ blogs })}
-    </div>
+    <Suspense fallback={<Spinner />}>
+      <div className={`flex ${blogs.length === 0 ? 'items-center' : ''} justify-center h-full`}>
+        {blogs.length === 0 ? (<div><p className="text-2xl">No result</p></div>) : <BlogGrid blogs={blogs} />}
+      </div>
+    </Suspense>
   );
 }
